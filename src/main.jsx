c8cb90b476c80554dcc366f9a5918f52cbbea4dd
proyecto colsoft <nicolasalferez20@gmail.com>
2026-06-02 16:57:49 -0500

    main

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { store } from './App/store'
+import { store } from './app/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
